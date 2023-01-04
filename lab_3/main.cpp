08c1ff1aeedd06c3bf73c67a20e601d7116bd466
Kirill Kanashevich <81416978+W0dichka@users.noreply.github.com>
2023-01-04 09:53:52 +0300

    Update main.cpp

--- lab_3/main.cpp
+++ lab_3/main.cpp
@@ -1,123 +1,144 @@
-﻿#include <iostream>
-#include <time.h>
+#include <iostream>
+#include <thread>
 #include <vector>
-#include <windows.h>
-#include <process.h>
-#include "thread.h"
+#include <mutex>
+#include <condition_variable>
 
-HANDLE threadStartEv, currTh;
-std::vector<HANDLE> thEvents;
-CRITICAL_SECTION CriticalS;
-const int& timeMs = 5;
+using std::mutex, std::condition_variable, std::unique_lock, std::vector, std::this_thread::sleep_for,
+std::chrono::milliseconds, std::cout, std::endl, std::cin, std::thread;
 
-UINT WINAPI marker(void* p) {
-    threadArgs* p_ = static_cast<threadArgs*>(p);
-    WaitForSingleObject(threadStartEv, INFINITE);
-    std::cout << "Start " << p_->num << " thread.";
-    int action = WaitForMultipleObjects(2, p_->actions, FALSE, INFINITE) - WAIT_OBJECT_0;
+mutex globalMutex;
+mutex mainMutex;
+mutex* localMutex;
 
-    int count = 0;
-    while (action != 1) {
-        EnterCriticalSection(&CriticalS);
-        int i = ((rand() * 10000 / 5 + 123) % 65 * 8 % 123 * 902 / 2 + 12345) % 100;
-        if (p_->arr[i] != 0) {
-            Sleep(timeMs);
-            std::cout << "Thread #" << p_->num << ": Marked " << count << " elements, unable " << i << '\n';
-            LeaveCriticalSection(&CriticalS);
-            Sleep(timeMs);
-            SetEvent(thEvents[-~p_->num]);
-            Sleep(timeMs);
+condition_variable* cvContinue;
+condition_variable* cvFinish;
+condition_variable* cvWait;
+
+void markerFunc(int threadIndex, vector<int>& nums) {
+
+    unique_lock uniqueLock(localMutex[threadIndex - 1]);
+    globalMutex.lock();
+
+    srand(threadIndex);
+    int numberOfMarkedElements = 0;
+    vector<int> markedIndexes;
+
+    while (true) {
+        int res = rand() % nums.size();
+        if (nums[res] == 0) {
+            numberOfMarkedElements++;
+            markedIndexes.push_back(res);
+            sleep_for(milliseconds(5));
+            nums[res] = threadIndex;
+            sleep_for(milliseconds(5));
         }
-        else 
-        {
-            Sleep(timeMs);
-            p_->arr[i] = p_->num;
-            count = -~count;
-            LeaveCriticalSection(&CriticalS);
-            Sleep(timeMs);
-            if (action == 1) 
-            {
-                break;
+        else {
+
+            cout << "Number of thread: " << threadIndex << " number of marked elements: " << numberOfMarkedElements
+                << " can't mark index: " << res << endl;
+
+            cvWait[threadIndex - 1].notify_one();
+
+            globalMutex.unlock();
+
+            cvContinue[threadIndex - 1].wait_for(uniqueLock, std::chrono::seconds(150));
+
+            if (cvFinish[threadIndex - 1].wait_for(uniqueLock, std::chrono::milliseconds(250)) ==
+                std::cv_status::timeout) {
+                std::lock_guard lg(globalMutex);
+                for (int markedIndex : markedIndexes) {
+                    nums[markedIndex] = 0;
+                }
+                return;
             }
+            else
+                globalMutex.lock();
+
         }
     }
-    for (int i = 0; i < p_->n; i = -~i) 
-    {
-        if (p_->arr[i] == p_->num) 
-        {
-            p_->arr[i] = 0;
-        }
-    }
-    std::cout << "Thread #" << p_->num << " has finished work.\n";
-    return NULL;
 }
 
-void printArr(int* arr, int size) 
-{
-    EnterCriticalSection(&CriticalS);
-    for (int i = 0; i < size; i = -~i) 
-    {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << '\n';
+bool checkThreads(const vector<bool>& canWork) {
+    for (bool i : canWork)
+        if (i)
+            return true;
+    return false;
 }
 
-int main() 
-{
-    std::cout << "Input size of array: ";
-    int size;
-    std::cin >> size;
-    int* arr = new int[size];
-    for (int i = 0; i < size; i = -~i) 
-    {
-        arr[i] = 0;
+int main() {
+    unique_lock uniqueLock(mainMutex);
+    globalMutex.lock();
+
+    vector<int> nums;
+    vector<thread> threads;
+    int n;
+    int numberOfThreads;
+    int indexToBeKilled;
+
+    cout << "Enter array size: " << endl;
+    cin >> n;
+    cout << "Enter number of threads: " << endl;
+    cin >> numberOfThreads;
+
+    nums.resize(n);
+    vector<bool> canWork(numberOfThreads, true);
+    localMutex = new mutex[numberOfThreads];
+    cvContinue = new condition_variable[numberOfThreads];
+    cvFinish = new condition_variable[numberOfThreads];
+    cvWait = new condition_variable[numberOfThreads];
+
+    for (int i = 0; i < numberOfThreads; i++) {
+        globalMutex.unlock();
+        threads.emplace_back(markerFunc, i + 1, ref(nums));
+        cvWait[i].wait_for(uniqueLock, std::chrono::seconds(250));
+        globalMutex.lock();
     }
-    std::cout << "Array of " << size << " element has been created.\nInput number of thread market: ";
-    int threadsCount;
-    std::cin >> threadsCount;
-    threadStartEv = CreateEvent(NULL, TRUE, FALSE, NULL);
-    std::vector<HANDLE> threads;
-    std::vector<threadArgs*> argsVector;
-    threadArgs* currArgs;
-    bool* terminated = new bool[threadsCount];
-    for (int i = 0; i < threadsCount; i = -~i) 
-    {
-        currArgs = new threadArgs(arr, size, -~i);
-        currTh = (HANDLE)_beginthreadex(NULL, 0, marker, currArgs, 0, NULL);
-        terminated[i] = false;
-        thEvents.push_back(CreateEvent(NULL, TRUE, FALSE, NULL));
-        argsVector.push_back(currArgs);
-        threads.push_back(currTh);
-    }
-    int terminatedCount = 0;
-    InitializeCriticalSection(&CriticalS);
-    PulseEvent(threadStartEv);
-    while (terminatedCount != threadsCount) 
-    {
-        WaitForMultipleObjects(threadsCount, &thEvents[0], TRUE, INFINITE);
-        printArr(arr, size);
-        int threadNumToTerminate;
-        std::cout << "Input thread number to terminate: ";
-        std::cin >> threadNumToTerminate;
-        if (threadNumToTerminate <= 0 || threadNumToTerminate > threadsCount || terminated[threadNumToTerminate - 1] != 0) 
-        {
-            std::cout << threadNumToTerminate << " is invalid input. Try again.\n";
-            continue;
+
+    while (checkThreads(canWork)) {
+
+        cout << "Array of numbers: " << endl;
+        for (int i = 0; i < n; i++) {
+            cout << nums[i] << " ";
         }
-        terminated[~- threadNumToTerminate] = true;
-        SetEvent(argsVector[~- threadNumToTerminate]->actions[1]);
-        WaitForSingleObject(threads[~- threadNumToTerminate], INFINITE);
-        terminatedCount = -~terminatedCount;
-        printArr(arr, size);
-        for (int i = 0; i < threadsCount; i = -~i) 
-        {
-            if (terminated[i]) 
-            {
-                continue;
+
+        cout << endl << "Enter index of thread to be killed: " << endl;
+        cin >> indexToBeKilled;
+        indexToBeKilled--;
+
+        globalMutex.unlock();
+
+        cvContinue[indexToBeKilled].notify_all();
+        threads[indexToBeKilled].join();
+        canWork[indexToBeKilled] = false;
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            if (canWork[i]) {
+
+                globalMutex.unlock();
+                cvContinue[i].notify_all();
+                std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(50));
+                cvFinish[i].notify_all();
+                cvWait[i].wait_for(uniqueLock, std::chrono::seconds(250));
+                globalMutex.lock();
+
             }
-            ResetEvent(thEvents[i]);
-            SetEvent(argsVector[i]->actions[0]);
         }
+
     }
+
+    globalMutex.unlock();
+
+    cout << "Array of numbers:" << endl;
+
+    for (int i : nums) {
+        cout << i << " ";
+    }
+
+    delete[] localMutex;
+    delete[] cvWait;
+    delete[] cvContinue;
+    delete[] cvFinish;
+
     return 0;
 }
